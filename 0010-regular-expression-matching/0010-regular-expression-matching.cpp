63baa940b255e8d3246eda74c2342ca32e59e6c1
Felipe Cardoso Vargas <128812730+Felipecardosovargas@users.noreply.github.com>
2024-07-19 10:37:08 -0300

    Time: 7 ms (33.12%), Space: 9.9 MB (18.89%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
+        
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+        
+        // Initialize for patterns that can match an empty string
+        for (int j = 1; j <= n; ++j) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2]; // Consider '*' as matching zero of the preceding element
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
