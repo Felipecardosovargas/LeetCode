9d12d659e8de13e804a55c9ff8b457ae7608317f
Felipe Cardoso Vargas <128812730+Felipecardosovargas@users.noreply.github.com>
2024-07-19 10:24:42 -0300

    Time: 725 ms (47.83%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,21 +1,34 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 
-WITH NodeTypes AS (
-    SELECT
-        t1.id,
-        CASE
-            WHEN t1.p_id IS NULL THEN 'Root'
-            WHEN t2.id IS NOT NULL THEN 'Inner'
-            ELSE 'Leaf'
-        END AS type
-    FROM
-        Tree t1
-    LEFT JOIN
-        Tree t2
-    ON t1.id = t2.p_id
+WITH ParentNodes AS (
+    SELECT DISTINCT p_id
+    FROM Tree
+    WHERE p_id IS NOT NULL
+),
+LeafNodes AS (
+    SELECT id
+    FROM Tree
+    MINUS
+    SELECT DISTINCT p_id AS id
+    FROM Tree
+    WHERE p_id IS NOT NULL
+),
+InnerNodes AS (
+    SELECT DISTINCT p_id AS id
+    FROM Tree
+    WHERE p_id IS NOT NULL
+),
+RootNodes AS (
+    SELECT id
+    FROM Tree
+    WHERE p_id IS NULL
 )
 
-SELECT id, type
-FROM NodeTypes
-GROUP BY id, type
+SELECT id,
+       CASE
+           WHEN id IN (SELECT id FROM RootNodes) THEN 'Root'
+           WHEN id IN (SELECT id FROM InnerNodes) THEN 'Inner'
+           ELSE 'Leaf'
+       END AS type
+FROM Tree
 ORDER BY id;
